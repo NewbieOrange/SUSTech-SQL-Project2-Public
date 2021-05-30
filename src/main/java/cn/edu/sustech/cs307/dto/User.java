23593c7bdbf1791c3167c7896146b5f478350fac
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-05-30 14:36:41 +0800

    Update definition for user's full name

--- src/main/java/cn/edu/sustech/cs307/dto/User.java
+++ src/main/java/cn/edu/sustech/cs307/dto/User.java
@@ -4,9 +4,10 @@ public abstract class User {
 
     public int id;
     /**
-     * A user's full name is: first_name || ' ' || last_name, if both first name and last name are alphabetical, otherwise first_name || last_name.
+     * A user's full name is: first_name || ' ' || last_name, if both first name and last name are alphabetical (English alphabets) or space (' '), otherwise first_name || last_name.
      *
-     * For example, if a user has first name David and last name Lee then the full name is David Lee; if another user has first name 张 and last name 三, the full name is 张三.
+     * For example, if a user has first name David and last name Lee then the full name is David Lee; if another user has first name 张 and last name 三, the full name is 张三;
+     * if first name 'David Lee' and last name 'Roth' then full name is 'David Lee Roth'.
      */
     public String fullName;
 }
