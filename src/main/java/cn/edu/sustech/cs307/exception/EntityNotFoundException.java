9565ed315efae5f561cf057b1589eca612a98f83
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-06-04 00:27:42 +0800

    Allow custom exceptions to have message or cause

--- src/main/java/cn/edu/sustech/cs307/exception/EntityNotFoundException.java
+++ src/main/java/cn/edu/sustech/cs307/exception/EntityNotFoundException.java
@@ -1,8 +1,18 @@
 package cn.edu.sustech.cs307.exception;
 
 /**
- *  in remove method, if there is no Entity about specific, throw EntityNotFoundException.
- *  in get method, if there is no Entity about specific id, throw EntityNotFoundException.
+ * in remove method, if there is no Entity about specific, throw EntityNotFoundException.
+ * in get method, if there is no Entity about specific id, throw EntityNotFoundException.
  */
 public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/main/java/cn/edu/sustech/cs307/exception/IntegrityViolationException.java
+++ src/main/java/cn/edu/sustech/cs307/exception/IntegrityViolationException.java
@@ -4,4 +4,14 @@ package cn.edu.sustech.cs307.exception;
  * In all add method, if some of parameters are invalid, throw IntegrityViolationException
  */
 public class IntegrityViolationException extends RuntimeException {
+    public IntegrityViolationException() {
+    }
+
+    public IntegrityViolationException(String message) {
+        super(message);
+    }
+
+    public IntegrityViolationException(Throwable cause) {
+        super(cause);
+    }
 }
